4b50ed94d3fe26b985bda0dba301f24e1742df23
Ziqian Li <liziqian1992@gmail.com>
2022-02-24 21:43:18 -0800

    pass the stringifyJSON test

--- spec/parseJSONSpec.js
+++ spec/parseJSONSpec.js
@@ -8,7 +8,7 @@ describe('parseJSON', function() {
       var equality = _.isEqual(result, expected); // why can't we use `===` here?
       // Replace this line with an `expect` statement that tests
       // the behavior described by the `it` string
-      throw new Error('Test is missing.')
+      throw new Error('Test is missing.');
     });
   });
 
@@ -17,8 +17,8 @@ describe('parseJSON', function() {
       var fn = function() {
         parseJSON(test);
       };
-      // if you'd prefer, you can write your version of parseJSON 
-      // so that it passes this test instead of the one on line 21. 
+      // if you'd prefer, you can write your version of parseJSON
+      // so that it passes this test instead of the one on line 21.
       // expect(parseJSON(test)).to.equal(undefined);
       expect(fn).to.throw(SyntaxError);
     });
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,37 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = [];
+
+  if (typeof obj === 'function' || obj === undefined) {
+    return '';
+  }
+  if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  }
+  if (obj === null || typeof obj === 'boolean' || typeof obj === 'number') {
+    return '' + obj;
+  }
+
+  if (Array.isArray(obj)) {
+    for (var i = 0; i < obj.length; i ++) {
+      result.push(stringifyJSON(obj[i]));
+    }
+    result = result.join(',');
+    return '[' + result + ']';
+  }
+
+  if (typeof obj === 'object') {
+    for (var key in obj) {
+      if (obj[key] === undefined || typeof obj[key] === 'function') {
+        continue;
+      } else {
+        result.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+      }
+    }
+    result = result.join(',');
+    return '{' + result + '}';
+  }
+
+  return result;
 };
